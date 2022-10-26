248f020583d5c108ad7573034406659fbdbf45e3
BlackBadgeMedia <oscar@blackbadgemedia.com>
2022-10-26 11:54:38 +0100

    r

--- main.py
+++ main.py
@@ -187,6 +187,7 @@ def main () -> None:
 
     students = entities.student.load_students('students.txt')
     teachers = entities.teacher.load_teachers('teachers.txt')
+    subjects = entities.subject.load_subjects('subjects.txt')
 
 
 if __name__ == '__main__':
