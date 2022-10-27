efbcd2bd2a869b7ab9252b72b5f5d70c1cd449f3
Rahul Surapaneni <rahuls14@yahoo.co.uk>
2022-10-27 16:44:04 +0100

    .

--- xlsx to txt.py
+++ xlsx to txt.py
@@ -11,26 +11,26 @@ Keep all of the program inside of functions so it can be accessed as an import f
 from openpyxl import load_workbook
 
 
-
 def xlsx_to_txt () -> None:
+
     """
     Reads the input spreadsheet and converts each page into text.
     Saves in individual text files.
     """
     
-    files = ['students.txt', 'teachers.txt', 'subjects.txt', 'rooms.txt'] #file name to output to
+    files = ['students.txt', 'teachers.txt', 'subjects.txt', 'rooms.txt'] #txt file name to output to
 
     for i in range(4):
         wb = load_workbook("IO\input.xlsx") #opens input.xlsx
-        wb.active=wb.worksheets[i]
+        wb.active=wb.worksheets[i] #selects sheet i
         sheet = wb.active
-        maxRow = sheet.max_row
-        maxColumn=sheet.max_column
-        for x in range(2, maxRow + 1):
-            for y in range(1, maxColumn +1):
-                cell_obj = sheet.cell(row=x, column=y)
-                print(cell_obj.value, end=' | ', file=open(files[i], 'a'))
-            print('', file=open(files[i], 'a'))
+        maxRow = sheet.max_row #finds final row
+        maxColumn=sheet.max_column #finds final column
+        for x in range(2, maxRow + 1): #iterates through rows
+            for y in range(1, maxColumn +1): #iterates through columns
+                cell_obj = sheet.cell(row=x, column=y) #selects cell
+                print(cell_obj.value, end=' | ', file=open(files[i], 'a')) #prints cell
+            print('', file=open(files[i], 'a')) #newline
 
 # write code in here if you only want it to be accessed when this file is run directly, not as an import
 def main () -> None:
