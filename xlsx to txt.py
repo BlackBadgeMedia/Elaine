6c8627d32bf9467abe33e8f504eb5c186e33ed5d
BlackBadgeMedia <oscar@blackbadgemedia.com>
2022-10-27 08:49:11 +0100

    Update xlsx to txt.py

--- xlsx to txt.py
+++ xlsx to txt.py
@@ -3,3 +3,8 @@ Takes the xl sheets with the information to text.
 
 Should read from input.xlsx in IO and output data to the texts files in the specified format. 
 """
+
+
+from openpyxl import Workbook
+
+
